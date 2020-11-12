a7c360e13cd0b35a7928d17677ab3f324afe0fa8
jvhti <jvhti@hotmail.com>
2020-11-12 00:56:23 -0300

    Create temporary ErrorHandler

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 var express = require('express');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const jsonErrorHandler = async (err, req, res) => {
+  res.status(500).json({error: err.toString(), status: 500});
+};
+
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.urlencoded({extended: false}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(jsonErrorHandler);
 
 module.exports = app;
