588c67cd9acdf92a6665039c2758e8f92f23d137
jvhti <jvhti@hotmail.com>
2020-11-12 12:19:37 -0300

    Implement GET /probe

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,12 @@ const probeViewer = require('../utils/probeViewer');
 const express = require('express');
 const router = express.Router();
 
+const probeNotFound = function (req, res, next) {
+  if (!req.session.currentProbe)
+    return res.status(404).json({error: "Não foi encontrado nenhuma sonda em ação!"});
+  next();
+};
+
 router.post('/probe', function (req, res) {
   const type = req.body.type || "Standard";
   try {
@@ -16,13 +22,15 @@ router.post('/probe', function (req, res) {
   }
 });
 
-router.delete('/probe', function (req, res) {
-  if (!req.session.currentProbe)
-    return res.status(404).json({error: "Não foi encontrado nenhuma sonda em ação!"});
-
+router.delete('/probe', probeNotFound, function (req, res) {
   const probe = req.session.currentProbe;
   req.session.currentProbe = null;
   res.status(200).json({probe: probeViewer(probe), deleted: true});
+});
+
+
+router.get('/probe', probeNotFound, function (req, res) {
+  res.status(200).json(probeViewer(req.session.currentProbe));
 });
 
 module.exports = router;
--- routes/index.test.js
+++ routes/index.test.js
@@ -2,6 +2,8 @@ const request = require('supertest');
 const session = require('supertest-session');
 const app = require('../app');
 
+const directionDictionary = require('../utils/directionMapper').dictionary;
+
 describe('Probe Endpoints', () => {
   describe("POST /probe", () => {
     it('should throw an error if the type doesn\'t exists', async function () {
@@ -45,6 +47,28 @@ describe('Probe Endpoints', () => {
       expect(res.body).toHaveProperty('probe');
       expect(res.body).toHaveProperty('deleted');
       expect(res.body.deleted).toBeTruthy();
+    });
+  });
+
+  describe('GET /probe', () => {
+    it('should return a 404 if the probe wasn\'t created', async function () {
+      const res = await request(app).get('/probe').send();
+      expect(res.statusCode).toStrictEqual(404);
+    });
+
+    it('should delete the probe if it already exists', async function () {
+      const testSesssion = session(app);
+
+      await testSesssion.post('/probe').send();
+      const res = await testSesssion.get('/probe').send();
+
+      expect(res.statusCode).toStrictEqual(200);
+      expect(res.body).toHaveProperty('x');
+      expect(res.body).toHaveProperty('y');
+      expect(res.body).toHaveProperty('face');
+      expect(res.body.x).not.toBeNaN();
+      expect(res.body.y).not.toBeNaN();
+      expect(Object.keys(directionDictionary)).toContain(res.body.face);
     });
   });
 });
